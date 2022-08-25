3a8614ab02318c149488235cd28bbff8e957a530
Nivedhitha D <nivedhithadm@gmail.com>
2022-08-26 04:13:56 +0530

    ND red overlay complete for mask

--- test_overlay_func.py
+++ test_overlay_func.py
@@ -4,8 +4,38 @@ import numpy as np
 from PIL import Image
 
 def imageOverlay(image1, image2):
+    
+
+    image2 //= 50
+    
+    print(image2)
+    print("\n\n\n\n")
+
+    image2[image2==1] = 0 
+    image2[image2==2] = 0
+    image2[image2==3] = 1
+    image2[image2==4] = 1
+    image2[image2==5] = 1
+    image2[image2==6] = 1
+    image2[image2==7] = 1
+    image2[image2==8] = 1
+    image2[image2==9] = 1
+
+    print(image2)
+
     image2 = cv2.resize(image2, (image1.shape[1], image1.shape[0]))
-    overlayOutput = cv2.addWeighted(image1, 0.8, image2, 0.2, 0)
+    b_channel = image2[:,:,:1]
+    # b_channel *= 25
+    g_channel = image2[:,:,1:2]
+    r_channel = image2[:,:,2:]
+    # print(gb_channel.shape)
+    image2_recolor = np.stack([
+        b_channel.squeeze(),
+        g_channel.squeeze(),
+        r_channel.squeeze()*255
+    ], axis=2)
+    print(image2_recolor.shape)
+    overlayOutput = cv2.addWeighted(image1, 0.6, image2_recolor, 0.4, 0)
     return overlayOutput
 
 image1 = cv2.imread(r"D:\\work\\nive\\SSN-College-Of-Engineering\\Extra-Curricular\\UWARL\\sih\\Code\\SatVision_MapViewer-Backend\\custom-0.png")
